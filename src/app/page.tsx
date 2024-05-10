17542516b3a27cc1fabff11d24f429c4a0e00213
Andrei Darapiyevich <darapwarsaw@gmail.com>
2024-05-10 23:30:53 +0200

    feat: add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import Hero from './../components/ui/hero/Hero';
 
 export default function Home() {
 	return (
-		<div>
-			<h1>HEllo</h1>
-			<Link href='/catalog'> go to catalog </Link>
+		<div className='bg-white pb-6 sm:pb-8 lg:pb-12'>
+			<Hero />
 		</div>
 	);
 }
