69621beb7e54b9f9918ffd7d9351c1ef4a9c1164
Andrei Darapiyevich <darapwarsaw@gmail.com>
2024-05-13 23:46:03 +0200

    feat: add catalog to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import Hero from './../components/ui/hero/Hero';
+import Catalog from './../components/ui/catalog/Catalog';
+import { ProductService } from './servises/product.service';
 
-export default function Home() {
+export const metadata: Metadata = {
+	title: 'Home page',
+	description: 'Home page description',
+};
+
+async function getProducts() {
+	const data = await ProductService.getAll();
+
+	return data;
+}
+
+export default async function Home() {
+	const data = await getProducts();
+
 	return (
 		<div className='bg-white pb-6 sm:pb-8 lg:pb-12'>
 			<Hero />
+			<Catalog products={data} isFull={false} />
 		</div>
 	);
 }
